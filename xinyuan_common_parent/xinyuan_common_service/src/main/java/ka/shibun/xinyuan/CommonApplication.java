808c3414ce71dd255e86eff06063b53c19d810a5
shibun0319h <shibun0319@gmail.com>
2019-03-01 19:06:20 +0800

    配置中心本地化,需要远程端的时候将本地文件上传到git配置中心即可

---
+++ xinyuan_common_parent/xinyuan_common_service/src/main/java/ka/shibun/xinyuan/CommonApplication.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: CommonApplication
+ * Author:   shibun
+ * Date:     2019/3/1 18:36
+ * Description: 启动类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package ka.shibun.xinyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ *
+ * 启动类
+ *
+ * @author shibun
+ * @create 2019/3/1
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class CommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class);
+    }
+}
